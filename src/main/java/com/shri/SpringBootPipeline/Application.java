d92b2755e60b4081f20243904aa51604abff4645
Shrikant Shinde <shindeshrikant1214@gmail.com>
2023-07-26 23:43:35 +0530

    first commit

---
+++ src/main/java/com/shri/SpringBootPipeline/Application.java
@@ -0,0 +1,20 @@
+package com.shri.SpringBootPipeline;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner{
+public static final Logger logger=LoggerFactory.getLogger(Application.class);
+	public static void main(String[] args) {
+		logger.info("Its a Continous Integration Job");;
+		SpringApplication.run(Application.class, args);
+	}
+@Override
+public void run(String...args)throws Exception{
+	logger.info("its my second logger");
+}
+}
